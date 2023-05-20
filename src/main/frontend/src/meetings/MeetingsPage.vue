9b92fe1d4a8dd86aba5649d95a94efcd1854af61
xxkasia <123232271+xxkasia@users.noreply.github.com>
2023-05-20 22:08:31 +0200

    Update MeetingsPage.vue

--- src/main/frontend/src/meetings/MeetingsPage.vue
+++ src/main/frontend/src/meetings/MeetingsPage.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <NewMeetingForm @added="addNewMeeting($event)"></NewMeetingForm>
+    <div class="error" v-if="idError">Spotkanie o podanej nazwie istnieje. Wprowadź inną nazwę</div>
     <span v-if="meetings.length == 0">Brak zaplanowanych spotkań.</span>
-
     <h3 v-else>Zaplanowane zajęcia ({{ meetings.length }})</h3>
     <MeetingsList :meetings="meetings"
                   :username="username"
@@ -19,80 +19,89 @@ import axios from "axios";
 
 export default {
   components: {NewMeetingForm, MeetingsList},
-  props: {username: String},
+  props: {
+    username: String
+  },
   data() {
     return {
       meetings: [],
-      message: '',
-      meetingCreated: false,
-    };
+      idError: false
+    }
   },
-  created() {
-      this.loadMeetings();
+  created: async function() {
+  let importedDbDataMeetings = [];
+  let importedDbParticipants = [];
+  await axios.get('/api/meetings')
+        .then(response => {
+          importedDbDataMeetings = response.data;
+            console.log(importedDbDataMeetings);
+            importedDbDataMeetings.forEach(element => {
+                axios.get('/api/meetings/' + element.id + '/participants')
+                    .then(response => {
+                          importedDbParticipants = response.data;
+                          let participants = [];
+                          importedDbParticipants.forEach(element => {
+                              participants.push(element.login);
+                          });
+                          element.participants = participants;
+
+                          this.meetings.push(element);
+                    })
+                    .catch(() => console.log('Uczestnicy spotkania [' + element.title + '] nie zostali pobrani z bazy danych'));
+                });
+        })
+        .catch(() => console.log('Spotkania nie zostały pobrane z bazy danych'));
   },
   methods: {
-      loadMeetings() {
-          axios
-              .get("/api/meetings", { params: { username: this.username } })
-              .then(response => {
-                  this.meetings = response.data;
-              })
-              .catch(error => {
-                  console.log("Nie udało się załadować spotkań.");
-              });
-      },
-      addNewMeeting(meeting) {
-          axios.post('/api/meetings', meeting)
-              .then(response => {
-                  console.log('Dodano spotaknie.');
-                  this.meetings.push(meeting);
-              })
-              .catch(response => console.log('Nie udało się dodać spotkania'))
-      },
-      addMeetingParticipant(meeting) {
-          this.meetings.at(this.meetings.indexOf(meeting)).participants.push(this.username);
-          axios.post('/api/meetings/' + meeting.id + '/participants', {login:this.username} )
-              .then(response => {
-                  console.log('Dodano uczestnika.');
-              })
-              .catch(response => console.log('Nie udało się dodać uczestnika'))
-      },
+    addNewMeeting(meeting) {
+      if(meeting.description == null) {
+        meeting.description = '';
+      }
+
+      axios.post('/api/meetings', meeting)
+          .then(response => {
+              meeting.id = response.data.id;
+              console.log('Dodano spotkanie [' + meeting.title + ']');
+              this.meetings.push(meeting);
+              this.idError = false;
+          })
+          .catch(() => {
+              this.idError = true;
+              console.log('Spotkanie o nazwie [' + meeting.title + '] już istnieje');
+          });
+      console.log(meeting);
+    },
+    addMeetingParticipant(meeting) {
+      const meetingIndex = this.meetings.indexOf(meeting);
+      const participantsList = this.meetings.at(meetingIndex).participants;
+      
+      axios.post('/api/meetings/' + meeting.id + '/participants', {login:this.username})
+          .then(response => console.log('Dodano uczestnika [' + this.username + '] do spotkania [' + meeting.title + ']'))
+          .catch(() => 'Uczestnik nie został dodany do spotkania [' + meeting.title + ']');
+      participantsList.push(this.username);
+      console.log(this.meetings);
+    },
     removeMeetingParticipant(meeting) {
-        this.meetings.at(this.meetings.indexOf(meeting)).participants.push(this.username);
-        axios.post('/api/meetings/' + meeting.id + '/participants', {login:this.username} )
-            .then(response => {
-                console.log('Dodano uczestnika do spotkania.');
-            })
-            .catch(response => console.log('Nie udało się dodać uczestnika do spoktania'))
+      axios.delete('/api/meetings/' + meeting.id + '/participants/' + this.username)
+          .then(response => console.log('Usunięto uczestnika [' + this.username + '] ze spotkania [' + meeting.title + ']'))
+          .catch(() => 'Uczestnik nie został usunięty ze spotkania [' + meeting.title +']');
+      meeting.participants.splice(meeting.participants.indexOf(this.username), 1);
     },
     deleteMeeting(meeting) {
-        axios.delete('/api/meetings/' + meeting.id)
-            .then(response => {
-                console.log('Usunięto spotkanie.');
-            })
-            .catch(response => console.log('Nie udało się usunąć spotkania'))
-        this.meetings.splice(this.meetings.indexOf(meeting), 1);
-    },
+      axios.delete('/api/meetings/' + meeting.id)
+          .then(response => console.log('Usunięto spotkanie [' + meeting.title + ']'))
+          .catch(() => 'Spotkanie [' + meeting.title + '] nie zostało usunięte');
+      this.meetings.splice(this.meetings.indexOf(meeting), 1);
+      this.error = false;
+    }
   }
 }
 </script>
 
-<style>
-.alert {
-    padding: 5px;
-    border: 2px solid green;
-    background: lightgreen;
-    font-size: 2em;
-    color: darkgreen;
-    text-align: center;
-}
-
-.alert-red {
-    padding: 5px;
-    font-size: 2em;
-    color: darkred;
-    border: 2px solid red;
-    background: lightcoral;
-    text-align: center;
+<style scoped>
+.error {
+  padding-top: -40px;
+  padding-bottom: 20px;
+  color: #F00;
 }
 </style>
