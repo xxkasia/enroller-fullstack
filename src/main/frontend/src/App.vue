28c62bf7e68f3feba891a1158d0016d0f0c522c1
xxkasia <123232271+xxkasia@users.noreply.github.com>
2023-05-20 13:54:42 +0200

    Update App.vue

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -4,56 +4,67 @@
 
     <div v-if="authenticatedUsername">
       <UserPanel :username="authenticatedUsername" @logout="logMeOut()"></UserPanel>
-      <MeetingsPage :username="authenticatedUsername"></MeetingsPage>
+      <MeetingsPage :username="authenticatedUsername" :meetings="meetings"></MeetingsPage>
     </div>
 
     <div v-else>
-        <button :class="signingUp ? 'button-outline' : ''"  @click = "signingUp = false">Logowanie</button>
-        <button :class="!signingUp ? 'button-outline' : ''" @click = "signingUp = true">Rejestracja</button>
+      <button :class="signingUp ? 'button-outline' : '' "@click="signingUp = false"> Logowanie </button>
+      <button :class="!signingUp ? 'button-outline' : '' "@click="signingUp = true"> Rejestracja </button>
 
-      <div v-if="" class ="alert">
-        {{ message }}
+      <div v-if="message" class="['alert', 'alert-' + (this.isError ? 'error' : 'success')]">
       </div>
+      <div :class="['alert', 'alert-' + (this.isError ? 'error' : 'success')]" v-if="message">
+        {{ message }}
+
+      </div>
+
       <LoginForm v-if="!signingUp" @login="(user) => logMeIn(user)"></LoginForm>
-      <LoginForm v-else @login="(user) => register(user)" button-label="Zaloz konto"></LoginForm>
+      <LoginForm v-else @login="(user) => register(user)" button-label="Załóż konto"></LoginForm>
     </div>
   </div>
 </template>
-
-
 
 <script>
 import "milligram";
 import LoginForm from "./LoginForm";
 import UserPanel from "./UserPanel";
 import MeetingsPage from "./meetings/MeetingsPage";
-import axios from 'axios';
+import axios from "axios"
+
 export default {
   components: {LoginForm, MeetingsPage, UserPanel},
   data() {
     return {
+      message: "",
       signingUp: false,
       authenticatedUsername: '',
-      message: '',
-      isPositive: false
+      meetings: [],
+      isError: false
     }
   },
+
   methods: {
     logMeIn(user) {
-      axios.post('/api/tokens', user)
+      axios.post("/api/tokens", user)
           .then(response => {
-            // jestes zalogowany
             this.authenticatedUsername = user.login;
             const token = response.data.token;
             axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
             axios.get('/api/meetings')
-                .then(response => console.log(response.data));
+                .then(response => {
+                  this.meetings = response.data;
+                })
+                .catch(response => {
+                  this.isError = true;
+                  this.message = ("Nie udało się pobrać listy spotkań")
+                })
+            this.isError = false;
+            this.message = ("Udało się zalogować")
           })
           .catch(response => {
-            // niepoprawne haslo
-            this.message = 'Nieudane logowanie'
-          });
-     // this.authenticatedUsername = user.login;
+            this.isError = true;
+            this.message = ("Nie udało się zalogować")
+          })
     },
     logMeOut() {
       this.authenticatedUsername = '';
@@ -63,13 +74,15 @@ export default {
     register(user) {
       axios.post('/api/participants', user)
           .then(response => {
-            this.message = 'Twoje konto zostalo zalozone'
+            this.isError = false;
+            this.message = ("Udało się założyć konto")
+
           })
           .catch(response => {
-            // nie udało sie
-            this.message = 'Taki uzytkownik juz istnieje'
+            this.isError = true;
+            this.message = ("Nie udało się założyć konta")
           });
-    }
+    },
   }
 }
 </script>
@@ -81,10 +94,17 @@ export default {
 }
 
 .alert {
+  border: 1px solid black;
+  font-size: 20px;
   padding: 5px;
-  margin: 30px;
-  background: lightgreen;
-  font-size: 2em;
-  border: 3px darkgreen;
+}
+
+.alert-success {
+  background: darkseagreen;
+  border-color: green;
+}
+.alert-error {
+  background: palevioletred;
+  border-color: darkred;
 }
 </style>
